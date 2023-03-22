87e2e17cb6885d0f5d5e0e42c3a69057f55fa76a
Raiane Oliveira <raiane.oliveira404@gmail.com>
2023-03-22 15:19:54 -0300

    feat: responsiveness

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,6 +10,7 @@ import {
    FileText,
    DotsThreeCircle,
    User,
+   Pencil,
 } from "phosphor-react";
 
 import { NavLink } from "react-router-dom";
@@ -22,40 +23,41 @@ export function Sidebar() {
          <nav className="main-navigation">
             <NavLink to="/">
                <House weight="fill" />
-               Home
+               <span>Home</span>
             </NavLink>
             <a href="">
                <Hash weight="fill" />
-               Explore
+               <span>Explore</span>
             </a>
             <a href="">
                <Bell />
-               Notifications
+               <span>Notifications</span>
             </a>
             <a href="">
                <EnvelopeSimple />
-               Messages
+               <span>Messages</span>
             </a>
             <a href="">
                <BookmarkSimple />
-               Bookmarks
+               <span>Bookmarks</span>
             </a>
             <a href="">
                <FileText />
-               Lists
+               <span>Lists</span>
             </a>
             <a href="">
                <User />
-               Profile
+               <span>Profile</span>
             </a>
             <a href="">
                <DotsThreeCircle />
-               More
+               <span>More</span>
             </a>
          </nav>
 
          <button className="new-tweet" type="button">
-            Tweet
+            <Pencil />
+            <span>Tweet</span>
          </button>
       </aside>
    );
